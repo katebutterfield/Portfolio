edff41e56b9294d3b201e57f684c7f9b0fa3bb7b
Kate Butterfield <katebutterfield@1184-MBP-Kate.local>
2019-05-16 22:27:21 -0700

    navbar scroll

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,12 @@ import './Header.css'
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
+    <nav id="navigation" class="navbar-fixed-top">
       <Link to="/"><img src={require('../images/kate-logo-svg.svg')} width="30"/></Link>
       <Link to="/design">Design</Link>
       <Link to="/about">About</Link>
       <Link to="/contact"><button>Contact Me 👋</button></Link>
+      </nav>
     </div>
   </div>
 )
@@ -21,5 +23,12 @@ Header.propTypes = {
 Header.defaultProps = {
   siteTitle: ``,
 }
+
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".navbar-fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
 
 export default Header
