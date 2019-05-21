645d5dc4c71e59695facdcb57846792e67d8aa5c
Kate Butterfield <katebutterfield@1184-MBP-Kate.local>
2019-05-21 16:17:36 -0700

    image fix

--- src/pages/design.js
+++ src/pages/design.js
@@ -10,7 +10,7 @@ const Design = () => (
     <div>
   <section class="projects">
 	<article>
-		<div class="thumbnail"><img src={require('../images/homepage-entelo.svg')}/>
+		<div class="thumbnail"><img src={require('../images/homepage-browser.png')}/>
     </div>
 		<div class="content">
 			<h2 class="project-tile">
