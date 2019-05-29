542eb3cc4b4a4c4a61e0ff718333062daaa2d00c
Kate Butterfield <katebutterfield@1184-MBP-Kate.local>
2019-05-29 11:46:44 -0700

    card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Card from '../components/card';
 
 const IndexPage = () => (
   <Layout>
@@ -16,19 +17,26 @@ const IndexPage = () => (
           <div className="Kate">Portfolio</div>
           <p>I solve real problems in tech using pixel perfect boxes and drop-shadows.</p>
           <Link to="/design">See for yourself →</Link>
-          <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill="white" >
-            <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s"
-            values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
-
-            M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;
-
-            M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
-
-            M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;" />
-            </path>
-          </svg>
-        </div>
+    </div>
+    </div>
+    </div>
+    <div className="Cards">
+    <div className="CardGroup">
+    <Card
+      title="Design System"
+      text="1 Project"
+      image={require('../images/design-system.jpg')}
+    />
+    <Card
+      title="Interaction Design"
+      text="1 Project"
+      image={require('../images/interaction-design.jpg')}
+    />
+    <Card
+      title="Mobile App Design"
+      text="2 Projects"
+      image={require('../images/mobile-design.jpg')}
+    />
     </div>
     </div>
   </Layout>
