09767267e481ec18126df944e947cf1d8f6de69f
Kate Butterfield <katebutterfield@1184-MBP-Kate.local>
2019-05-29 12:11:58 -0700

    nav bar scrolled

--- src/components/header.js
+++ src/components/header.js
@@ -3,16 +3,44 @@ import PropTypes from "prop-types"
 import React from "react"
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={require('../images/kate-logo-svg.svg')} width="30"/></Link>
-      <Link to="/design">Design</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact"><button>Contact Me <span class="wave">👋</span></button></Link>
-    </div>
-  </div>
-)
+
+class Header extends React.Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+    hasScrolled: false
+  }
+}
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/kate-logo-svg.svg')} width="30"/></Link>
+          <Link to="/design">Design</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact"><button>Contact Me <span class="wave">👋</span></button></Link>
+        </div>
+      </div>
+    )
+  }
+}
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
