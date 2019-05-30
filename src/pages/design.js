caf5eea70c59f848beca4c4897f335be8e721dd5
Kate Butterfield <katebutterfield@1184-MBP-Kate.local>
2019-05-30 15:57:52 -0700

    added design projects

--- src/pages/design.js
+++ src/pages/design.js
@@ -10,7 +10,7 @@ const Design = () => (
     <div>
   <section class="projects">
 	<article>
-		<div class="thumbnail"><img src={require('../images/notifications.png')}/>
+		<div class="thumbnail"><img src={require('../images/homepage-cropped.png')}/>
     </div>
 		<div class="content">
 			<h2 class="project-tile">
@@ -26,6 +26,52 @@ const Design = () => (
             <div class="tags">InVision</div>
             <div class="tags">UX</div>
             <div class="tags">UI</div>
+        </div>
+			</div>
+			<Link to="/">View Project</Link>
+		</div>
+	</article>
+
+  <article>
+		<div class="thumbnail2"><img src={require('../images/form-tabs.gif')}/>
+    </div>
+		<div class="content">
+			<h2 class="project-tile">
+				<span class="background-wrapper">
+					Form Builder —
+				</span>
+			</h2>
+			<p>I created a self-serving form builder for EmployStream's web app that cut down on time spent training new users. </p>
+			<div class="skills">
+				<div>Tools</div>
+        <div class="tags-group">
+				    <div class="tags">Sketch</div>
+            <div class="tags">Anima</div>
+            <div class="tags">Animation</div>
+            <div class="tags">Lucky Orange</div>
+        </div>
+			</div>
+			<Link to="/">View Project</Link>
+		</div>
+	</article>
+
+  <article>
+		<div class="thumbnail3"><img src={require('../images/hsad.png')}/>
+    </div>
+		<div class="content">
+			<h2 class="project-tile">
+				<span class="background-wrapper">
+					Athletic Director Mobile App —
+				</span>
+			</h2>
+			<p>I mocked up an app design for HSAD Network, a communication platform for Athletic Directors.</p>
+			<div class="skills">
+				<div>Tools</div>
+        <div class="tags-group">
+				    <div class="tags">Sketch</div>
+            <div class="tags">Illustrator</div>
+            <div class="tags">Photoshop</div>
+            <div class="tags">Grid Layout</div>
         </div>
 			</div>
 			<Link to="/">View Project</Link>
