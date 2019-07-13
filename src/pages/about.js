03a4dfc45c110b50f555cda1c372f40203dac2a1
Kate Butterfield <katebutterfield@1184-MBP-Kate.local>
2019-07-12 18:29:57 -0700

    about updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,10 +24,24 @@ const About = () => (
 			<span class="user-position"> Product Designer</span>
 			<p class="location">📍 San Francisco </p>
 			<div class="social">
-      <a href="https://www.linkedin.com/in/kate-butterfield/"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-      <a href="https://dribbble.com/katebutter"><FontAwesomeIcon icon={['fab', 'dribbble']} /></a>
-      <a href="https://www.behance.net/butterk28f56"><FontAwesomeIcon icon={['fab', 'behance']} /></a>
+      <a href="https://www.linkedin.com/in/kate-butterfield/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+      <a href="https://dribbble.com/katebutter" target="_blank"><FontAwesomeIcon icon={['fab', 'dribbble']} /></a>
+      <a href="https://www.behance.net/butterk28f56" target="_blank"><FontAwesomeIcon icon={['fab', 'behance']} /></a>
       </div>
+      </div>
+      <div class="info-card">
+        <div class="intro">
+          <h3>Background</h3>
+          <div class="line"></div>
+          <p>I'm an <span class="highlight">enthusiastic designer</span>, currently working on improving the product experience at <a href="https://www.entelo.com/" target="_blank">Entelo</a>. My background in Interactive Media and Graphic Design allows me to build creative solutions while leveraging the power of technology.
+          <br/>
+          <br/>
+          I'm a naturally collaborative person and value my relationships with product stakeholders, teammates, and users. <span class="highlight">I'm driven by innovation and excited by the opportunity to help scale a solution by making informed and deliberate design decisions. </span>
+          <br/>
+          <br/>
+          In my free time you can find me exploring San Francisco, grabbing coffee, or reading up on design trends from some of my <a href="https://muz.li/" target="_blank">'secret source'</a>. 
+          </p>
+        </div>
       </div>
       </div>
     </div>
